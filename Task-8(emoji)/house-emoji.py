746bab0da09921c9f2ef178e3dbff35261f66633
chiragvispute <chiragvispute30@gmail.com>
2025-08-18 23:07:23 +0530

    task8+answer

---
+++ Task-8(emoji)/house-emoji.py
@@ -0,0 +1,12 @@
+import emoji  
+
+house_emojis = {
+    "Gryffindor": ":lion:",
+    "Slytherin": ":snake:",
+    "Ravenclaw": ":eagle:",
+    "Hufflepuff": ":badger:"
+}
+
+#  TODO 1: Take input from the user (house name)  
+#  TODO 2: Look up the emoji code for that house  
+#  TODO 3: Print the house name with its emoji using emoji.emojize()
