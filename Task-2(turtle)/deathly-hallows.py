f785bf110dc3d820d353f71fddf33ac6112a6f3d
chiragvispute <chiragvispute30@gmail.com>
2025-08-18 21:53:26 +0530

    task-1,2,3+answers

---
+++ Task-2(turtle)/deathly-hallows.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Harry Potter Glasses")
+
+pen = turtle.Turtle()
+pen.pensize(3)
+
+# TODO 1: Draw the left circle
+# TODO 2: Draw the right circle
+# TODO 3: Bridge between the circles
+
+turtle.done()
---
+++ Task-3(Pyjokes)/riddikulus.py
@@ -0,0 +1,6 @@
+# Pyjokes Documentation: https://pypi.org/project/pyjokes/
+# Task: Print a random joke when a user casts the spell "Riddikulus".
+
+import pyjokes
+
+# TODO: Use pyjokes to print a random joke when user types "Riddikulus"
