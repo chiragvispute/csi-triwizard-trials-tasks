f42ccd489be631344a6488cdc4337882b0b9d58e
chiragvispute <chiragvispute30@gmail.com>
2025-08-18 21:58:35 +0530

    task4+answer

---
+++ Task-4(matplotlib)/houses.py
@@ -0,0 +1,14 @@
+# Matplotlib Documentation: https://matplotlib.org/stable/users/explain/quick_start.html
+# Task: 
+# 1. Create a bar chart of students per house 
+#    (Gryffindor, Slytherin, Ravenclaw, Hufflepuff).
+# 2. Create a horizontal bar chart of the same data.
+
+import matplotlib.pyplot as plt  
+
+houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+students = [50, 45, 40, 35]
+
+# TODO 1: Create a bar chart of houses vs students
+# TODO 2: Create a horizontal bar chart
+# TODO 3: Display the charts
